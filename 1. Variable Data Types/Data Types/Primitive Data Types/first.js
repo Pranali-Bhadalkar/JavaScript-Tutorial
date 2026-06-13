0c01f56049b3cfc7156b1b615bad55cc8641e6f1
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-13 22:29:37 +0530

    Create first.js

---
+++ 1. Variable Data Types/Data Types/Primitive Data Types/first.js
@@ -0,0 +1,24 @@
+name = "Pranali Hanmant Bhadalkar";
+console.log(name);
+
+age = 21;
+console.log(age);
+
+price = 99.98;
+console.log(price);
+
+radius = 14;
+console.log(radius);
+
+a = null;
+console.log(a);
+
+y = undefined;
+console.log(y);
+
+isFollow=true;
+console.log(isFollow);
+
+let x = BigInt("123");
+console.log(x);
+ 
