0f56b9b171899fc6e5f41baa3bd3f6d2458663b4
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-13 22:48:24 +0530

    Create first.js

---
+++ 1. Variable Data Types/Data Types/Non-Primitive Data Types/Object/first.js
@@ -0,0 +1,11 @@
+const student =
+{
+    fullName : "Pranali Hanmant Bhadalkar",
+    age : 21,
+    cgpa : 8.76,
+    isPass : true
+};
+ 
+student ["name"]= "Pranali Hanmant Bhadalkar";
+
+console.log(student["name"]);
