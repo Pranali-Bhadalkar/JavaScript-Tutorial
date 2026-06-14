3f0ef98251c85052acdbe073b27b96c45a7a2e1c
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-14 22:20:59 +0530

    Create script.js

---
+++ 2. Operators & Conditional Statements/Operators/Comparison Operators/script.js
@@ -0,0 +1,34 @@
+
+//Assignment Operators 
+
+ let a=5;
+ let b=9;
+ console.log("a=",a, "& b=",b);
+
+ // ==
+ console.log("a==b ", a==b);
+
+  // !=
+ console.log("5!=9", a!=b);
+
+  // ===
+ console.log("a===b", a===b);
+
+  // !==
+ console.log("5!==9", a!==b);
+ 
+  // >
+ console.log("a>b", a>b);
+
+ // >=
+ console.log("5>=9", a>=b);
+
+ // <
+ console.log("a<b", a<b);
+
+ // <=
+ console.log("5<=9", a<=b);
+ 
+
+
+ 
