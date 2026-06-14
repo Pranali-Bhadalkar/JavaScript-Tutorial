75d895d5971e81648c77e42e07409f860a9bcc74
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-14 18:01:21 +0530

    Create script.js

---
+++ 2. Operators & Conditional Statements/Comments in Js/script.js
@@ -0,0 +1,21 @@
+
+//This is a single line comment
+const product =
+{
+    title : "Ball Pen",
+    rating : 4,
+    offer : 5,
+    price: 270
+};
+console.log(product);
+
+/*This is a multi-lined
+comment used in the javaScript*/
+const student =
+{
+    name : "Pranali",
+    rollno : 2103,
+   branch :"CSE",
+    marks: 82.17
+};
+console.log(student["name"]);
