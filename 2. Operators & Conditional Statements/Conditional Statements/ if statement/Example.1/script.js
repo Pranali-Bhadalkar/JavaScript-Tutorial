81ebc04b5fb6b0cf5b080688c7ef9a711050a1b1
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-14 22:59:23 +0530

    Create script.js

---
+++ 2. Operators & Conditional Statements/Conditional Statements/ if statement/Example.1/script.js
@@ -0,0 +1,19 @@
+
+//Conditional Statements
+
+// if statement
+
+ let age = 2;
+
+  if (age>=18)
+  {
+    console.log("Eligible for voting")
+  }
+
+ if (age<18)
+  {
+    console.log("Not eligible for voting")
+  }
+
+
+ 
