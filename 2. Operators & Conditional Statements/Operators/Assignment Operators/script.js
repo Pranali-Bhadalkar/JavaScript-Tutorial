57160da487385f696e92c65023eeef174f0e4a9a
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-14 22:13:09 +0530

    Create script.js

---
+++ 2. Operators & Conditional Statements/Operators/Assignment Operators/script.js
@@ -0,0 +1,34 @@
+ 
+//Assignment Operators 
+
+ let a=5;
+ let b=9;
+ console.log("a=",a, "& b=",b);
+
+ // += (a=a+8)
+ a+=8;
+ console.log("a= ", a);
+
+  // -= (a=a-8)
+ a-=8;
+ console.log("a= ", a);
+
+  // *= (a=a*8)
+ a*=8;
+ console.log("a= ", a);
+
+  // /= (a=a/8)
+ a/=8;
+ console.log("a= ", a);
+ 
+  // %= (a=a%8)
+ a%=8;
+ console.log("a= ", a);
+
+ // **= (a=a**8)
+ a**=8;
+ console.log("a= ", a);
+
+
+
+ 
