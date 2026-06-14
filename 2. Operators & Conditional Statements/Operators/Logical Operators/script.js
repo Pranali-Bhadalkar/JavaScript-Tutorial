7241ca8c2aa48dce79cebfe147b3e2197072be17
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-14 22:35:44 +0530

    Create script.js

---
+++ 2. Operators & Conditional Statements/Operators/Logical Operators/script.js
@@ -0,0 +1,22 @@
+
+//Logical Operators 
+
+ let a=7;
+ let b=5;
+ console.log("a=",a, "& b=",b);
+
+ //  Logical AND &&
+ let cond1=a>b;
+ let cond2= b==5;
+ console.log("cond1 && cond2 =", cond1 && cond2);
+
+  
+  // Logical OR ||
+  let cond3=a<b;
+ let cond4= b==7;
+ console.log("cond3 || cond4 =", a<b || b==7);
+
+  // Logical NOT !
+ console.log("!(7<5)", !(a<b));
+ 
+ 
