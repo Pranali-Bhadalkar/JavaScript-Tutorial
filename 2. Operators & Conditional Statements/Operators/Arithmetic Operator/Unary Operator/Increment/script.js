ad1c9b7d998d7657b4360d0938b14192347af520
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-14 19:04:42 +0530

    Create script.js

---
+++ 2. Operators & Conditional Statements/Operators/Arithmetic Operator/Unary Operator/Increment/script.js
@@ -0,0 +1,25 @@
+
+/*Arithmetic Operators(Unary Operator)
+Increment*/
+ let a=15;
+ let b=7;
+ console.log("a=",a, "& b=",b);
+ a=a+1;
+ console.log("a= ", a);
+
+ a++;
+ console.log("a= ", a);
+
+
+ //a++
+ console.log("a++=",a++);
+ console.log("a= ", a);
+
+ //++a
+ console.log("++a=",++a);
+
+
+
+
+
+ 
