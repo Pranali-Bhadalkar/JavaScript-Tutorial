2045b5af5ac8b9ba33f1004e63979a52a68810f4
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-14 23:31:05 +0530

    Create script.js

---
+++ 2. Operators & Conditional Statements/Conditional Statements/ esle-if Statement/Example.1/script.js
@@ -0,0 +1,31 @@
+
+//Conditional Statements
+
+// else-if statement
+
+ let mode="green";
+ let color;
+
+  if (mode==="dark")
+  {
+    color="dark";
+  }
+  else if
+  (mode==="blue")
+  {
+    color="blue";
+  }
+ else if
+  (mode==="pink")
+  {
+    color="pink";
+  }
+  else
+  {
+    color="white";
+  }
+    console.log(color);
+  
+
+
+ 
