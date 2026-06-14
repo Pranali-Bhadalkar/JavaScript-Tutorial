bdb2c7b1b9f1ff456b8efff8dc5c6cb3a7630680
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-14 19:09:52 +0530

    Create script.js

---
+++ 2. Operators & Conditional Statements/Operators/Arithmetic Operator/Unary Operator/Decrement/script.js
@@ -0,0 +1,25 @@
+
+/*Arithmetic Operators(Unary Operator)
+Decrement*/
+ let a=15;
+ let b=7;
+ console.log("a=",a, "& b=",b);
+ a=a-1;
+ console.log("a= ", a);
+
+ a--;
+ console.log("a= ", a);
+
+
+ //a--(Post-Decrement)
+ console.log("a--=",a--);
+ console.log("a= ", a);
+
+ //--a(Pree-Decrenment)
+ console.log("--a=",--a);
+
+
+
+
+
+ 
