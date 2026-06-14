cae866cccda69a390eb590acf563f638bf311ec6
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-14 23:15:56 +0530

    Create script.js

---
+++ 2. Operators & Conditional Statements/Conditional Statements/ if -esle Statement/Example.2/script.js
@@ -0,0 +1,18 @@
+
+//Conditional Statements
+
+// if-else statement
+
+ let age=28
+
+  if (age>=18)
+  
+  {
+   console.log("Eligible for voting");
+  }
+  else
+  {
+    console.log("Not eligible for voting");
+  }
+
+ 
