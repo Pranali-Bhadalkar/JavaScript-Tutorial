38bfa77018b009a5de47ab10b81b52af7a761ff4
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-16 20:05:59 +0530

    Add grading logic based on score
    Implement grading system based on score ranges.

---
+++ 2. Operators & Conditional Statements/Conditional Statements/Activity/script.js
@@ -0,0 +1,36 @@
+
+//Activity
+
+ let score=69;
+ let grade;
+
+ if(score>=91 && score<=100)
+ {
+  grade="A";
+ }
+
+ else if(score>=81 && score<=90)
+  {
+  grade="B";
+ }
+
+ else if(score>=71 && score<=80)
+ {
+  grade="C";
+ }
+
+ else if(score>=61 && score<=70)
+  {
+  grade="D";
+ }
+
+ else if(score>=51 && score<=60)
+  {
+  grade="E";
+ }
+ else if(score>=0 && score<=50)
+  {
+  grade="F";
+ }
+
+ console.log(" Grade is :", grade);
