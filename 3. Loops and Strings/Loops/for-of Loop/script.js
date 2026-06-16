c09691133a61b0f2b0170aefd48e5bb31770393a
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-16 22:16:20 +0530

    Create script.js

---
+++ 3. Loops and Strings/Loops/for-of Loop/script.js
@@ -0,0 +1,15 @@
+
+//Loops
+//for-of Loop
+let str="Pranali Hanmant Bhadalkar";
+let size=0;
+for(let i of str)
+{
+  console.log("i=",i);
+  size++;
+}
+
+  console.log("string size=", size);
+  
+
+  
