0afae3da27b1192bc16efd141878adf1d8e5098b
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-16 22:29:29 +0530

    Create script.js

---
+++ 3. Loops and Strings/Loops/for-in Loop/script.js
@@ -0,0 +1,22 @@
+
+//Loops
+//for-in Loop
+let student=
+{
+name:"Pranali Hanmant Bhadalkar",
+rollNo:2103,
+age:21,
+cgpa:8.76,
+isPass:true,
+};
+
+for(let key in student)
+{
+  console.log("skey=",key, "value=",student[key]);
+  
+}
+
+
+  
+
+  
