aeb8e5c16202be847885c304ab0ef0860a932d13
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-17 21:52:45 +0530

    Create script.js

---
+++ Arrays/Array Methods /splice()/script.js
@@ -0,0 +1,14 @@
+
+//Arrays
+
+let arr=[1,2,3,4,5,6,7,8,9,];
+arr.splice(2,101,102);
+ 
+//Add Element
+//arr.splice(2,0,101);
+
+//Delete Element
+//arr.splice(3,1);
+
+//Replace Element
+//arr.splice(3,1,101);
