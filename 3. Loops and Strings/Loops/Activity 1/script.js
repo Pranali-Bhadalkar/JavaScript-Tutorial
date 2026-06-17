fbfecfc5103709add375e562b62f66a12e51002b
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-17 11:29:18 +0530

    Create script.js

---
+++ 3. Loops and Strings/Loops/Activity 1/script.js
@@ -0,0 +1,12 @@
+
+//Loops
+let gameNum=25;
+let userNum=prompt("Guess the game number:");
+while(userNum != gameNum)
+{
+  userNum=prompt("You entered wrong number. Guess again:");
+}
+
+console.log("Conratulations, you entered the right number");
+
+  
