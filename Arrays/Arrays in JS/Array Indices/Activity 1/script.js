a76c20341947e7358ab3120ecab8b38d70d17854
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-17 21:09:30 +0530

    Create script.js

---
+++ Arrays/Arrays in JS/Array Indices/Activity 1/script.js
@@ -0,0 +1,12 @@
+
+//Arrays
+
+let marks=[85,97,44,37,76,60];
+let sum=0;
+for(let val of marks)
+{
+    sum = sum+val;
+}
+let avg=sum/marks.length;
+
+console.log(`Avg marks of the class = ${avg}`);
