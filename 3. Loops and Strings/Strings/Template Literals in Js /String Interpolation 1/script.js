e66a8f554027ce7b8a54972c47a59fc6abe5b786
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-17 12:13:26 +0530

    Create script.js

---
+++ 3. Loops and Strings/Strings/Template Literals in Js /String Interpolation 1/script.js
@@ -0,0 +1,11 @@
+
+//Strings
+//Template Literals
+
+let product =
+{
+    title:"Pencile",
+    price:15,
+};
+let output=`The cost of ${product.title} is ${product.price} rupees`;
+console.log(output);
