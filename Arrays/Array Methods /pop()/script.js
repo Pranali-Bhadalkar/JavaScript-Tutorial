fd3c9f074629e758d121dad8567772288fe947f1
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-17 21:24:37 +0530

    Create script.js

---
+++ Arrays/Array Methods /pop()/script.js
@@ -0,0 +1,8 @@
+
+//Arrays
+
+let foodItems=["potato", "apple", "tomato", "banana"];
+    console.log(foodItems);
+   let deletedItem=foodItems.pop();
+   console.log(foodItems);
+   console.log("deleted", deletedItem);
