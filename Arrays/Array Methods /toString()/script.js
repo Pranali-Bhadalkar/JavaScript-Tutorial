42b6421a0bb6f5c570dcda0edd6968a7d306421a
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-17 21:28:54 +0530

    Create script.js

---
+++ Arrays/Array Methods /toString()/script.js
@@ -0,0 +1,9 @@
+
+//Arrays
+
+let foodItems=["potato", "apple", "tomato", "banana"];
+let marks=[97,86,73,92];
+    console.log(foodItems);
+   console.log(foodItems.toString());
+   console.log(foodItems);
+  
