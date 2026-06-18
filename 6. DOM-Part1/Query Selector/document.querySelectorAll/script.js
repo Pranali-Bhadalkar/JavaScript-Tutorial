072edc1fe01338721788f51878744b3b907204f1
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-18 22:00:35 +0530

    Add script to query and log DOM elements

---
+++ 6. DOM-Part1/Query Selector/document.querySelectorAll/script.js
@@ -0,0 +1,12 @@
+
+let firstEl=document.querySelectorAll("p");
+console.dir(firstEl);
+
+
+    
+let secondEl=document.querySelectorAll(".myClass");
+console.dir(secondEl);
+
+
+   let thirdEl=document.querySelectorAll(".myId");
+console.dir(thirdEl);
