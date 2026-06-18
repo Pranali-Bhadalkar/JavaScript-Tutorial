0b6074a25d1e99756ee11798cd3be6ede901fbff
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-18 12:03:00 +0530

    Create script.js

---
+++ 5. Functions/Array Methods/Map/script.js
@@ -0,0 +1,14 @@
+
+    let num=[67, 52,39];
+    let newArr = num.map((val) =>
+    {
+        return val*2;
+    });
+    console.log(newArr);
+    let calcSquare = (num) =>
+    {
+        console.log(num*num);
+    };
+
+
+    
