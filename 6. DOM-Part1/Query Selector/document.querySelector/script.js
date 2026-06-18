d9e7e14f32932da7d1cb46b5ab6ae4cc80c58fb8
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-18 21:55:23 +0530

    Create script.js

---
+++ 6. DOM-Part1/Query Selector/document.querySelector/script.js
@@ -0,0 +1,12 @@
+
+let firstEl=document.querySelector("p");
+console.dir(firstEl);
+
+
+    
+let secondEl=document.querySelector(".myClass");
+console.dir(secondEl);
+
+
+   let thirdEl=document.querySelector(".myId");
+console.dir(thirdEl);
