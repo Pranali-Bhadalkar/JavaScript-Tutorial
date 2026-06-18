4c5df9ce7e1c1bd87260b8d7b91950798da34773
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-18 11:38:37 +0530

    Create script.js

---
+++ 5. Functions/Functions in JS/Activity/script.js
@@ -0,0 +1,17 @@
+
+//Functions
+
+function countVowels(str)
+{
+    let count = 0;
+    for (const char of str)
+    {
+        if(char==="a"  || char==="e" || char==="i" || char==="o" ||char==="u")
+          {
+            count++;
+          } 
+    }
+    console.log(count);
+}
+
+    
