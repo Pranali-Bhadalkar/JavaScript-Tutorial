199d034b0f56dfb702c61d5950460a2fd58d75ba
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-18 22:53:45 +0530

    Create script.js

---
+++ 7. DOM-Part2/DOM Manipulation/Style/script.js
@@ -0,0 +1,15 @@
+
+let div=document.querySelector("div");
+
+div.style.backgroundColor="green";
+
+div.style.fontSize="26px";
+
+div.innerText="Hello!";
+
+div.style.display="flex";
+
+div.style.justifyContent="center";
+
+div.style.alignItems="center";
+
