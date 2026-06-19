a244cfd616eded9fbb424ec20f2e8520af569630
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-19 19:22:35 +0530

    Create app.js

---
+++ 10. Classes & Objects/Inheritance in JS/app.js
@@ -0,0 +1,37 @@
+class Person
+{
+    constructor()
+    {
+        this.species="homo sapiens";
+    }
+
+    eat()
+    {
+        console.log("eat");
+    }
+
+    sleep()
+    {
+        console.log("sleep");
+    }
+
+    work()
+    {
+        console.log("do nothing");
+    }
+}
+
+class Engineer extends Person{
+work() 
+{
+console.log("solve problems, build something");
+}
+}
+
+
+class Doctor extends Person{
+work() 
+{
+console.log("treat patients");
+}
+}
