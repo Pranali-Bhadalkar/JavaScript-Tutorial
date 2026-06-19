566aa673e1fa1fae41b4b2a1815900c409aa7874
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-19 12:24:23 +0530

    Create script.js

---
+++ 8. Events/Events in JS/ Event Listeners/node.addEventListener/script.js
@@ -0,0 +1,14 @@
+let button1=document.querySelector("#button1");
+
+button1.addEventListener("click",() =>
+{
+    console.log("button1 was clicked");
+});
+
+
+button1.addEventListener("click",() =>
+{
+    console.log("button1 was clicked - handler2");
+});
+
+let div = document.querySelector("div");
