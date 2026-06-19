bd88b09ca85183058d3e259afa277addaf0d187f
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-19 11:20:54 +0530

    Add event handling for button and div elements

---
+++ 8. Events/Events in JS/Event Object/script.js
@@ -0,0 +1,18 @@
+let button1=document.querySelector("#button1");
+button1.onclick=(evt) =>
+{
+    console.log(evt);
+    console.log(evt.type);
+    console.log(evt.target);
+    console.log(evt.clientX, evt.clientY);
+};
+
+
+let div=document.querySelector("div");
+div.onmouseover=(evt) =>
+{
+   console.log(evt);
+    console.log(evt.type);
+    console.log(evt.target);
+    console.log(evt.clientX, evt.clientY);
+};
