84735ec3ea5edd9a09d17fd643ca3b37f659207a
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-19 19:05:21 +0530

    Create app.js

---
+++ 10. Classes & Objects/Classes in JS/app.js
@@ -0,0 +1,24 @@
+class ToyotaCar
+{
+    constructor(brand, mileage)
+    {
+        console.log("creating new object");
+        this.brand=brand;
+        this.mileage=mileage;
+    }
+
+    start()
+    {
+        console.log("start");
+    }
+
+    STOP()
+    {
+        console.log("stop");
+    }
+}
+
+let fortuner = new ToyotaCar("fortuner",10);
+console.log("fortuner");
+let lexus = new ToyotaCar("lexus",12);
+console.log("lexus");
