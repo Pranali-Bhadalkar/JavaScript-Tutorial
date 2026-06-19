0a6adacfb962066259d1d81ba5d3f39ad24aa70c
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-19 18:48:36 +0530

    Create app.js

---
+++ 10. Classes & Objects/Prototypes in JS/app.js
@@ -0,0 +1,18 @@
+const employee =
+{
+    calcTax()
+    {
+        console.log("tax rate is 10%");
+    },
+};
+
+const karanArjun =
+{
+    salary:50000,
+    calcTax()
+    {
+        console.log("tax rate is 20%");
+    },
+};
+
+karanArjun__proto__=employee;
