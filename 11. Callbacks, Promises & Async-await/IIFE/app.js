dc0f12b8036cae1ff2fc38d6e2613d341008bcf4
Pranali-Bhadalkar <pranalibhadalkar@gmail.com>
2026-06-20 10:10:47 +0530

    Create app.js

---
+++ 11. Callbacks, Promises & Async-await/IIFE/app.js
@@ -0,0 +1,34 @@
+ function getData(dataId)
+{
+    return new Promise((resolve, reject) =>
+    {
+            setTimeout(() =>
+            {
+            console.log("data", dataId);
+            resolve("success");
+        }, 2000);
+    });
+}
+
+//Async-await
+async function getAllData()
+    {
+       console.log("getting data1....");
+       await getData(1);
+       console.log("getting data2....");
+       await getData(2);
+       console.log("getting data3....");
+       await getData(3);
+}
+
+(async function()
+{
+    console.log("getting data1....");
+       await getData(1);
+       console.log("getting data2....");
+       await getData(2);
+       console.log("getting data3....");
+       await getData(3);
+    })();
+
+
